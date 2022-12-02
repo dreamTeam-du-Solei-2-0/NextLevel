9a60c0cb0be28c23eda0d6eaf6a73bb427fdce94
King-Of-Programer <alexdichemix@gmail.com>
2022-12-02 11:21:37 +0200

    regex for card

--- Next Level/Pages/Cart.xaml.cs
+++ Next Level/Pages/Cart.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,6 +29,41 @@ namespace Next_Level.Pages
             if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
             {
                
+            }
+        }
+
+        private void NumberCard_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if ((new Regex(@"[^0-9.-]+")).IsMatch(NumberCard.Text))
+            {
+                NumberCard.Foreground = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                NumberCard.Foreground = new SolidColorBrush(Colors.White);
+                
+            }
+            if(string.IsNullOrEmpty(NumberCard.Text))
+            {
+                NumberCard.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+        }
+
+        private void DateCard_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if ((new Regex(@"^(0[13578]|1[02])/([012]\d|3[01])|(0[469]|11)/([012]\d|30)|02/[012]\d$")).IsMatch(DateCard.Text))
+            {
+                DateCard.Foreground = new SolidColorBrush(Colors.White);
+            }
+            else
+            {
+                DateCard.Foreground = new SolidColorBrush(Colors.Red);
+               
+
+            }
+            if (string.IsNullOrEmpty(NumberCard.Text))
+            {
+                DateCard.BorderBrush = new SolidColorBrush(Colors.Red);
             }
         }
     }
