638c752f1231290b26f62b11a527ed3b509e6d78
dsgnrr <75548640+dsgnrr@users.noreply.github.com>
2022-12-04 20:59:05 +0200

    changing the display of products
    -Products load from file
    -New desgin for product display

--- Next Level/Classes/Product.cs
+++ Next Level/Classes/Product.cs
@@ -7,57 +7,67 @@ using System.Xaml.Permissions;
 using Next_Level.Classes;
 
 namespace Next_Level.Classes
-{   
+{
+
     [Serializable]
     public class Product
     {
-        string path;
+        public string Id { get; set; }
+        public string productPhoto { get; set; }
+        public string productName { get; set; }
+        public double productPrice { get; set; }
+        public int productCount { get; set; }
 
-        
-
-        public string Name { get; set; }
-
-        public double NewPrice { get; set; }
-
-        public double OldPrice { get; set; }
-
-        public string Description { get; set; }
-
-        public string Image { get; set; }
-
-        public Dictionary<int, string> Comments { get; set; }
-
-        public bool isAvailable { get; set; }
-
-        public int Views  { get; set; }
-
-        public int FrequentlyPurchasedCounter { get; set; }
-
-        public DateTime PlacementDate { get; set; }
-
-        public int ID_Product { set; get; }
 
         public Product()
         {
-
-            ID_Product++;
-
-        }
-        
-
-        public void InFile() {
-            path = ID_Product.ToString();
-            XmlFormat product_xml = new XmlFormat(path);
-            
-        }
-
-
-        public Product(string name, double new_price, string image)
-        {
-            
-            this.Name = name;
-            this.NewPrice = new_price;
-            this.Image = image;
+            Id = string.Empty;
+            productPhoto = string.Empty;
+            productName = string.Empty;
+            productPrice = 0;
+            productCount = 0;
         }
     }
+
+    #region before_edit
+    //[Serializable]
+    //public class Product
+    //{
+    //    string path;
+
+
+
+    //    public string Name { get; set; }
+
+    //    public double NewPrice { get; set; }
+
+    //    public double OldPrice { get; set; }
+
+    //    public string Description { get; set; }
+
+    //    public string Image { get; set; }
+
+    //    public Dictionary<int, string> Comments { get; set; }
+
+    //    public bool isAvailable { get; set; }
+
+    //    public int Views  { get; set; }
+
+    //    public int FrequentlyPurchasedCounter { get; set; }
+
+    //    public DateTime PlacementDate { get; set; }
+
+    //    public int ID_Product { set; get; }
+
+    //    public Product()
+    //    {
+
+    //        ID_Product++;
+
+    //    }
+
+
+
+    //}
+    #endregion
 }
---
+++ Next Level/Classes/ProductList.cs
@@ -0,0 +1,123 @@
+﻿using Next_Level.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Next_Level.Classes
+{
+    public class ProductList
+    {
+        List<Product> products;
+        IFile file = null;
+        public bool fileLoad { get; set; } = false; 
+        string product_path = @"..\Debug\Products.xml";
+
+        public ProductList()
+        {
+            products = Load();
+        }
+
+        string generateId()
+        {
+            string result = string.Empty;
+            Random random = new Random();
+            int[] id = new int[5];
+            while (true)
+            {
+                result = string.Empty;
+                for (int i = 0; i < 5; i++)
+                {
+                    id[i] = random.Next(0, 9);
+                    result += id[i];
+                }
+                if (!idIsUnique(result))
+                    break;
+            }
+
+            return result;
+        }
+
+        public void AddNew(Product product)
+        {
+            product.Id = "nl" + generateId();
+            products.Add(product);
+            Save();
+        }
+        public void removeUser(Product product)
+        {
+            products.Remove(product);
+            Save();
+        }
+        //Возвращает список товаров которые содержат похожие имена
+        public List<Product> getProductByName(string productName)
+        {
+            List<Product> find = new List<Product>();
+            foreach (var product in products)
+            {
+                if (product.productName.Contains(productName))
+                    find.Add(product);
+            }
+            if (find.Count != 0)
+                return find;
+            else return null;
+        }
+
+        public List<Product> getProductsByPrice(double productPrice)
+        {
+            List<Product> find = new List<Product>();
+            foreach (var product in products)
+            {
+                if (product.productPrice <= productPrice)
+                    find.Add(product);
+            }
+            if (find.Count != 0)
+                return find;
+            else return null;
+        }
+
+        public Product getProductById(string productId)
+        {
+            foreach (var product in products)
+            {
+                if (product.Id == productId)
+                    return product;
+            }
+            return null;
+        }
+
+        public bool idIsUnique(string id)
+        {
+            foreach (var product in products)
+            {
+                if (product.Id == id)
+                    return true;
+            }
+            return false;
+        }
+
+        public IEnumerator<Product> GetEnumerator()
+        {
+            for (int i = 0; i < products.Count; i++)
+                yield return products[i];
+        }
+        void Save()
+        {
+            file = new XmlFormat(product_path);
+            file.Save(products);
+        }
+        List<Product> Load()
+        {
+            if (File.Exists(product_path))
+            {
+                file = new XmlFormat(product_path);
+                fileLoad = true;
+                return file.Load<List<Product>>();
+            }
+            fileLoad = false;
+            return new List<Product>();
+        }
+    }
+}
