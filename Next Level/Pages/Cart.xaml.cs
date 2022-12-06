9c017bdcc2d31e7614e688a50dc5452d380cf5e2
King-Of-Programer <alexdichemix@gmail.com>
2022-12-06 14:45:15 +0200

    regex for card(only Visa)

--- Next Level/Pages/Cart.xaml.cs
+++ Next Level/Pages/Cart.xaml.cs
@@ -34,13 +34,13 @@ namespace Next_Level.Pages
 
         private void NumberCard_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if ((new Regex(@"[^0-9.-]+")).IsMatch(NumberCard.Text))
+            if ((new Regex(@"^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}(?:2131|1800|35\d{3})\d{11})$")).IsMatch(NumberCard.Text)) // проверка карты только для виза у визы начиется с 4 номер
             {
-                NumberCard.Foreground = new SolidColorBrush(Colors.Red);
+                NumberCard.Foreground = new SolidColorBrush(Colors.White);
             }
             else
             {
-                NumberCard.Foreground = new SolidColorBrush(Colors.White);
+                NumberCard.Foreground = new SolidColorBrush(Colors.Red);
                 
             }
             if(string.IsNullOrEmpty(NumberCard.Text))
@@ -51,7 +51,7 @@ namespace Next_Level.Pages
 
         private void DateCard_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if ((new Regex(@"^(0[13578]|1[02])/([012]\d|3[01])|(0[469]|11)/([012]\d|30)|02/[012]\d$")).IsMatch(DateCard.Text))
+            if ((new Regex(@"^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$")).IsMatch(DateCard.Text)) // проверка даты ввод(0524 или 05/24)
             {
                 DateCard.Foreground = new SolidColorBrush(Colors.White);
             }
@@ -64,6 +64,25 @@ namespace Next_Level.Pages
             if (string.IsNullOrEmpty(NumberCard.Text))
             {
                 DateCard.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+        }
+
+        private void CVVCard_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+             if ((new Regex(@"^([0-9]{3,4})$")).IsMatch(CVVCard.Text)) // проверка CVV
+            {
+                CVVCard.Foreground = new SolidColorBrush(Colors.White);
+            }
+            else
+            {
+                CVVCard.Foreground = new SolidColorBrush(Colors.Red);
+
+
+            }
+            if (string.IsNullOrEmpty(NumberCard.Text))
+            {
+                CVVCard.BorderBrush = new SolidColorBrush(Colors.Red);
             }
         }
     }
