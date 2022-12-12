039afac03b3638861d2ba28d9c3a78ebed35501d
dsgnrr <75548640+dsgnrr@users.noreply.github.com>
2022-12-12 16:15:47 +0200

    Added photo to Order

--- Next Level/Pages/Order.xaml.cs
+++ Next Level/Pages/Order.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Next_Level.Classes;
+using Next_Level.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,17 +19,78 @@ using System.Windows.Shapes;
 
 namespace Next_Level.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для Order.xaml
-    /// </summary>
     public partial class Order : Page
     {
         Home relative_page;
+        User current_user;
+        Accounts accounts = new Accounts();
+        IFile file;
+
+        ProductList _products;
+        Product product;
+        List<Feedback> feedbacks;
         public Order(string id,Home home)
         {
             InitializeComponent();
             relative_page = home;
+            LoadCurrentUser();
+            LoadProductList();
+            LoadProduct(id);
         }
+
+        #region LOAD_ALL_DATA
+
+        //загрузка текущего пользователя
+        void LoadCurrentUser()
+        {
+            string path_currentUser = NextLevelPath.CURRENT_USER;
+            file = new BinnaryFile(path_currentUser);
+            current_user = accounts.getUserByLogin(file.Load<string>());
+        }
+
+        //подгружает продукты из бд
+        void LoadProductList() => _products = new ProductList();
+
+        //находит нужный продукт
+        void LoadProduct(string id)
+        {
+            product = _products.getProductById(id);
+            if (product == null)
+                product = new Product();
+            else
+            {
+                LoadPriceDescription();
+                LoadImage();
+            }
+        }
+
+        //подгружает описание и цену из бд
+        void LoadPriceDescription()
+        {
+            if (product.descriptionProduct == string.Empty)
+                Description.Text = "No description";
+            else
+                Description.Text = product.descriptionProduct;
+
+            TotalPrice.Text = "Price: " + product.productPrice.ToString();
+        }
+
+        //подгружает фото из бд
+        void LoadImage()
+        {
+            string target = NextLevelPath.STOREBD_PATH;
+            target = System.IO.Path.GetFullPath(target);
+            target = System.IO.Path.Combine(target, product.productName);
+            target = System.IO.Path.Combine(target, product.productPhoto);
+            if (File.Exists(target))
+            {
+                var productPhoto = createImageBox();
+                productPhoto.Source = loadPhoto(target);
+                gridPhoto.Children.Add(productPhoto);
+            }
+        }
+
+        #endregion
 
         #region EVENTS
 
@@ -34,6 +99,33 @@ namespace Next_Level.Pages
             relative_page.LoadProducts();
         }
 
+        #endregion
+
+        #region CREATE_ELEMENTS
+
+        Image createImageBox()
+        {
+            Image imageBox = new Image();
+            imageBox.Height = 200;
+            imageBox.Width = 200;
+            return imageBox;
+        }
+
+        //загружает фото
+        BitmapImage loadPhoto(string path)
+        {
+            BitmapImage img = new BitmapImage();
+            if (File.Exists(path))
+            {
+                img.BeginInit();
+                img.UriSource = new Uri(path, UriKind.RelativeOrAbsolute);
+                img.DecodePixelWidth = 200;
+                img.DecodePixelHeight = 200;
+                img.EndInit();
+                return img;
+            }
+            return null;
+        }
         #endregion
     }
 }
